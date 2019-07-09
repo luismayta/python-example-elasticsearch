c903c80d252f13a15b00de989e352b05b1787efa
@slovacus <slovacus@gmail.com>
2019-07-09 14:39:10 -0500

    (#3) test: implement fixture load

---
+++ conftest.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from blog.fixtures.core import *  # noqa pylint: disable=W0614,W0401
+from blog.fixtures.db import *  # noqa pylint: disable=W0614,W0401
+from blog.fixtures.data import *  # noqa pylint: disable=W0614,W0401
